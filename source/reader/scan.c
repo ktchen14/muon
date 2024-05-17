a8caf78d56697c430c5cf08e52e318c19564a1ce
Kaiting Chen <ktchen14@gmail.com>
2024-05-17 09:53:28 -0400

    Remove symbol files

--- source/reader/scan.c
+++ source/reader/scan.c
@@ -6,9 +6,8 @@
 #include "../common.h"
 
 #include "syntax.h"
-#include "symbol.h"
 
-#include <muon/status.h>
+#include "../status.h"
 
 /*!conditions:re2c*/
 
@@ -25,6 +24,12 @@ typedef struct {
   enum YYCONDTYPE condition;
 } scan_t;
 
+typedef struct {
+  yytoken_kind_t kind;
+  YYSTYPE yylval;
+  YYLTYPE yylloc;
+} symbol_t;
+
 /// Scan and return the next symbol in the @a buffer
 static yytoken_kind_t scan_next(
     const YYCTYPE *restrict buffer, scan_t *scan, YYSTYPE *yylval)
@@ -33,6 +38,9 @@ static yytoken_kind_t scan_next(
 /// Advance the scan @a cursor
 static void cursor_next(
     const YYCTYPE *restrict buffer, cursor_t *cursor)
+  __attribute__((nonnull));
+
+static void symbol_debug(FILE *stream, const symbol_t *symbol)
   __attribute__((nonnull));
 
 void scan_debug(const YYCTYPE *string, const char *name) {
@@ -106,4 +114,73 @@ static void cursor_next(
     cursor->column = 1;
   } else
     cursor->column++;
+}
+
+static void symbol_debug(FILE *stream, const symbol_t *symbol) {
+  printf("%s:%zu:%zu [%zu + %zu]: ",
+      symbol->yylloc.name != NULL ? symbol->yylloc.name : "(none)",
+      symbol->yylloc.line,
+      symbol->yylloc.column,
+      symbol->yylloc.offset,
+      symbol->yylloc.length);
+
+  if (symbol->kind > YYEOF && symbol->kind < YYerror) {
+    int c = symbol->kind;
+    switch (c) {
+      case '\n':
+        printf("\\n");
+        break;
+      case ' ':
+        printf("_");
+        break;
+      default:
+        printf("%c", c);
+        break;
+    }
+  } else switch (symbol->kind) {
+    case YYEOF:
+      printf("EOF\n");
+      break;
+    case YYerror:
+      printf("Error\n");
+      break;
+    case CONSTANT:
+      printf("\"constant\"");
+      break;
+    case INSTANCE:
+      printf("\"instance\"");
+      break;
+    case TYPE:
+      printf("\"type\"");
+      break;
+    case NAME:
+      fputs("NAME ", stdout);
+      fwrite(symbol->yylval.text.c, symbol->yylval.text.length, 1, stdout);
+      break;
+    case BOOLEAN:
+      printf("BOOLEAN %s", symbol->yylval.boolean ? "true" : "false");
+      break;
+    case INTEGER:
+      printf("INTEGER %lli", symbol->yylval.integer);
+      break;
+    case STRING:
+      printf("STRING \"");
+      for (size_t i = 0; i < symbol->yylval.text.length; i++) {
+        char c = symbol->yylval.text.c[i];
+        switch (c) {
+          case '"': printf("\\\""); break;
+          case '\t': printf("\\t"); break;
+          case '\n': printf("\\n"); break;
+          case '\r': printf("\\r"); break;
+          default: printf("%c", c); break;
+        }
+      }
+      printf("\"");
+      break;
+    default:
+      printf("Unknown %d", symbol->kind);
+      break;
+  }
+
+  printf("\n");
 }
---
+++ source/status.h
@@ -0,0 +1,6 @@
+#ifndef MU_STATUS_I
+#define MU_STATUS_I
+
+#include <muon/status.h>
+
+#endif /* MU_STATUS_I */
