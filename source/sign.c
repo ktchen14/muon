5151323f3cf21862406a932d7c6a1ff84aa445d6
Kaiting Chen <ktchen14@gmail.com>
2024-07-01 09:49:28 -0400

    Some cleanup

--- source/sign.c
+++ source/sign.c
@@ -1,5 +1,13 @@
 #include "sign.h"
 
 void mu_sign_debug(const mu_sign_t *sign) {
-  switch (sign->kind) { MU_EACH_SIGN_KIND(MU_ABSTRACT_SIGN_CALL, sign, mu, debug) }
+  switch (sign->kind) {
+#define MU_EMIT(lower, upper, _) \
+    case MU_##upper##_SIGN: \
+      mu_##lower##_sign_debug((const mu_##lower##_sign_t *) sign); \
+      break;
+
+    MU_EACH_SIGN_KIND(MU_EMIT)
+#undef MU_EMIT
+  }
 }
--- source/sign.h
+++ source/sign.h
@@ -10,12 +10,4 @@
 #include "sign/variable.h"  // IWYU pragma: export
 #include "sign/vector.h"    // IWYU pragma: export
 
-/**
- * @brief Emit a @c case in an abstract call on an sign function
- */
-#define MU_ABSTRACT_SIGN_CALL(lower, upper, t, variable, prefix, name, ...) \
-  case MU_##upper##_SIGN: \
-    prefix##_##lower##_sign_##name((const mu_##lower##_sign_t *) sign, ##__VA_ARGS__); \
-    break;
-
 #endif /* MU_SIGN_I */
