c4bd513cfba97bb4293de15d671b297498ff4ad2
Kaiting Chen <ktchen14@gmail.com>
2024-07-10 18:58:20 -0400

    Update comment

--- header/muon/type.h
+++ header/muon/type.h
@@ -6,6 +6,7 @@
 #include "type/integer.h"  // IWYU pragma: export
 #include "type/vector.h"   // IWYU pragma: export
 
+/// Emit debugging information on the abstract @a type to the debug stream
 void mu_type_debug(const mu_type_t *type) __attribute__((nonnull));
 
 #endif /* MU_TYPE_H */
