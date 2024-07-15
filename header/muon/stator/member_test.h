a063607a9cb0e19dcd2116e553065936874d4ede
Kaiting Chen <ktchen14@gmail.com>
2024-07-15 00:03:03 -0400

    Try to implement access node

--- header/muon/stator/member_test.h
+++ header/muon/stator/member_test.h
@@ -3,16 +3,18 @@
 
 #include "abstract_test.h"  // IWYU pragma: export
 
+#include "abstract_type.h"
 #include "name.h"
 
 typedef struct {
   MU_TEST_HEADER;
 
   const mu_name_t *name;
+  const mu_type_t *type;
 } mu_member_test_t;
 
 const mu_member_test_t *mu_member_test(
-    mu_engine_t *engine, const mu_name_t *name)
+    mu_engine_t *engine, const mu_name_t *name, const mu_type_t *type)
   __attribute__((malloc, nonnull));
 
 void mu_member_test_debug(const mu_member_test_t *test)
--- source/stator/member_test.c
+++ source/stator/member_test.c
@@ -3,6 +3,7 @@
 #include "engine.h"
 #include "name.h"
 #include "test.h"
+#include "type.h"
 
 #include <assert.h>
 #include <stddef.h>
@@ -10,8 +11,9 @@
 #include <string.h>
 
 const mu_member_test_t *mu_member_test(
-    mu_engine_t *engine, const mu_name_t *name) {
+    mu_engine_t *engine, const mu_name_t *name, const mu_type_t *type) {
   assert(name->as_stator.engine == engine);
+  assert(type->as_stator.engine == engine);
 
   size_t size = sizeof(mu_member_test_t);
 
@@ -19,7 +21,7 @@ const mu_member_test_t *mu_member_test(
   if ((result = test_allocate(engine, size)) == NULL)
     return NULL;
   *result = (mu_member_test_t) {
-    .as_test.kind = MU_MEMBER_TEST, .name = name,
+    .as_test.kind = MU_MEMBER_TEST, .name = name, .type = type,
   };
 
   return assign_test(engine, result);
