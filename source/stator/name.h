30a95f111e8ea79c7c9722448dd4cde95b22e983
Kaiting Chen <ktchen14@gmail.com>
2024-07-17 12:40:12 -0400

    Add name_cmp and Wno-parentheses

--- source/stator/name.h
+++ source/stator/name.h
@@ -3,7 +3,11 @@
 
 #include <muon/stator/name.h>  // IWYU pragma: export
 
+#include "../common.h"
+
+#include <assert.h>
 #include <stddef.h>
+#include <string.h>
 
 /// Used to traverse a name prefix chain
 typedef struct {
@@ -21,6 +25,17 @@ static inline name_cursor_t *name_cursor(const mu_name_t *name) {
   static const size_t offset = offsetof(name_header_t, name);
   name_header_t *header = (name_header_t *) ((char *) name - offset);
   return &header->cursor;
+}
+
+/// Compare name @a a to name @a b
+__attribute__((nonnull, pure))
+static inline int name_cmp(const mu_name_t *a, const mu_name_t *b) {
+  assert(a->as_stator.engine == b->as_stator.engine);
+
+  int result;
+  if ((result = memcmp(a->text, b->text, minimum(a->length, b->length))) != 0)
+    return result;
+  return (a->length > b->length) - (b->length > a->length);
 }
 
 #endif /* MU_STATOR_NAME_I */
