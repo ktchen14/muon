aaa646efb2fa9233df312f382ebc7f272c043186
Kaiting Chen <ktchen14@gmail.com>
2025-03-02 15:56:37 -0500

    Couple more changes

--- source/inductor/induce.c
+++ source/inductor/induce.c
@@ -145,15 +145,13 @@ void debug_type(const type_t *type) {
       break;
 
     case SCHEME_TYPE:
-      if (type->polymorphic_length > 0) {
-        fprintf(stderr, "∀ (");
-        for (size_t i = 0; i < type->polymorphic_length; i++) {
-          if (i > 0)
-            fprintf(stderr, ", ");
-          debug_variable_type_name(type->polymorphic[i]);
-        }
-        fprintf(stderr, ") ");
+      fprintf(stderr, "∀ (");
+      for (size_t i = 0; i < type->polymorphic_length; i++) {
+        if (i > 0)
+          fprintf(stderr, ", ");
+        debug_variable_type_name(type->polymorphic[i]);
       }
+      fprintf(stderr, ") ");
 
       debug_type(type->matter);
       break;
@@ -746,11 +744,11 @@ __attribute__((nonnull)) static const type_t *invoke_expr_induce(
   const type_t *lambda = induce_reveal(induce, &expr->lambda->as_node);
   const type_t *matter = induce_reveal(induce, &expr->matter->as_node);
 
-  if (lambda->kind == SIMPLE_TYPE && lambda->core == induce->lambda_core) {
-    if (restrict_type(induce, matter, lambda->argv[0]) == NULL)
-      return NULL;
-    return lambda->argv[1];
-  }
+  /* if (lambda->kind == SIMPLE_TYPE && lambda->core == induce->lambda_core) { */
+  /*   if (restrict_type(induce, matter, lambda->argv[0]) == NULL) */
+  /*     return NULL; */
+  /*   return lambda->argv[1]; */
+  /* } */
 
   const type_t *result;
   if ((result = variable_type(induce, scheme)) == NULL)
@@ -914,6 +912,9 @@ __attribute__((nonnull, pure)) static const type_t *define_stmt_induce(
 
     type = next;
   }
+
+  if (polymorphic_length == 0)
+    return expr_type;
 
   const type_t *result;
   if ((result = scheme_type(induce, expr_type, polymorphic_length, polymorphic)) == NULL)
