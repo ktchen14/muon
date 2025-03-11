189857076d62a69d998417822c9fb14d3fe3fe27
Kaiting Chen <ktchen14@gmail.com>
2025-03-11 14:52:06 -0400

    Delete unused

--- source/inductor/reduce.c
+++ source/inductor/reduce.c
@@ -42,7 +42,8 @@ const mu_coercion_t *make_coercion(
   assert(edge != NULL);
 
   const tactic_t *tactic = edge->tactic;
-  assert(tactic != NULL);
+  if (tactic == NULL)
+    return &induce->id_coercion->as_coercion;
 
   if (tactic->kind == RECORD_TACTIC) {
     const record_tactic_t *record_tactic = (const record_tactic_t *) tactic;
@@ -94,33 +95,6 @@ const mu_type_t *coerce_to_lower(induce_t *induce, const mu_variable_type_t *typ
   const mu_join_type_t *result;
   if (rare((result = join_type_activate(allocation)) == NULL))
     return NULL;
-
-  /* // Add an edge for each constituent type to record the coercion to the join */
-  /* // type. */
-  /* j = 0; */
-  /* for (size_t i = 0; i < induce->edge_length; i++) { */
-  /*   induce_edge_t edge = induce->edge[i]; */
-  /*   if (edge.upper == &type->as_type && edge.lower->kind != MU_VARIABLE_TYPE) { */
-  /*     const mu_join_coercion_t *join_coercion; */
-  /*     if ((join_coercion = mu_join_coercion(j)) == NULL) */
-  /*       return NULL; */
-  /*     if (append_edge(induce, edge.lower, &result->as_type, &join_coercion->as_coercion) == NULL) */
-  /*       return NULL; */
-  /*     j++; */
-  /*   } */
-  /* } */
-
-  /* for (size_t j = 0; j < result->argc; j++) { */
-  /*   const mu_type_t *argument = result->argv[j]; */
-
-  /*   for (size_t i = 0; i < induce->edge_length; i++) { */
-  /*     induce_edge_t edge = induce->edge[i]; */
-  /*     if (edge.lower == argument && edge.upper != &result->as_type && edge.upper->kind != MU_VARIABLE_TYPE) { */
-  /*       if (append_edge(induce, &result->as_type, edge.upper, edge.coercion) == NULL) */
-  /*         return NULL; */
-  /*     } */
-  /*   } */
-  /* } */
 
   ((mu_variable_type_t *) type)->assignment = &result->as_type;
 
