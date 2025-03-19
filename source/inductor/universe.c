7dfdd033c55d5630f3c7c10ba5038fa7012f0faf
Kaiting Chen <ktchen14@gmail.com>
2025-03-19 15:23:04 -0400

    Add universe

---
+++ source/inductor/universe.c
@@ -0,0 +1,53 @@
+#include "universe.h"
+
+#include <errno.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+universe_t *universe_initialize(universe_t *universe) {
+  universe_edge_t *data;
+  size_t volume = 1;
+  if ((data = malloc(sizeof(universe_edge_t[volume]))) == NULL)
+    return NULL;
+  *universe = (universe_t) { .volume = 1, .data = data };
+  return universe;
+}
+
+const universe_edge_t *universe_search(
+    const universe_t *universe, const mu_type_t *source, const mu_type_t *target) {
+  for (size_t i = 0; i < universe->length; i++) {
+    const universe_edge_t *edge = &universe->data[i];
+    if (edge->lower == source && edge->upper == target)
+      return edge;
+  }
+  return NULL;
+}
+
+const universe_edge_t *universe_append(
+    universe_t *universe,
+    const mu_type_t *restrict source,
+    const mu_type_t *restrict target,
+    const tactic_t *tactic) {
+  if (universe->length >= universe->volume) {
+    size_t volume = universe->volume;
+    if (rare(__builtin_mul_overflow(volume, 2, &volume)))
+      return errno = ENOMEM, NULL;
+
+    size_t size;
+    if (rare(__builtin_mul_overflow(volume, sizeof(universe_edge_t), &size)))
+      return errno = ENOMEM, NULL;
+
+    universe_edge_t *data;
+    if ((data = realloc(universe->data, size)) == NULL)
+      return NULL;
+    universe->data = data;
+
+    universe->volume = volume;
+  }
+
+  universe_edge_t *result = &universe->data[universe->length++];
+  *result = (universe_edge_t) {
+    .lower = source, .upper = target, .tactic = tactic,
+  };
+  return result;
+}
---
+++ source/inductor/universe.h
@@ -0,0 +1,39 @@
+#ifndef MU_INDUCTOR_UNIVERSE_I
+#define MU_INDUCTOR_UNIVERSE_I
+
+#include "type.h"
+#include "tactic.h"
+
+#include <stddef.h>
+
+typedef struct {
+  const mu_type_t *lower;
+  const mu_type_t *upper;
+  const tactic_t *tactic;
+  _Bool direct;
+} universe_edge_t;
+
+typedef struct {
+  size_t length;
+  size_t volume;
+  universe_edge_t *data;
+} universe_t;
+
+/// Initialize the @a universe
+universe_t *universe_initialize(universe_t *universe)
+  __attribute__((nonnull));
+
+const universe_edge_t *universe_search(
+    const universe_t *universe, const mu_type_t *source, const mu_type_t *target)
+  __attribute__((nonnull));
+
+/// Create an edge @a source → @a target in the @a universe
+const universe_edge_t *universe_append(
+    universe_t *universe,
+    const mu_type_t *restrict source,
+    const mu_type_t *restrict target,
+    const tactic_t *tactic);
+
+typedef universe_edge_t induce_edge_t;
+
+#endif /* MU_INDUCTOR_UNIVERSE_I */
