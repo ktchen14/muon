968a8f821911403ee4398646e354b3b14a665aec
Kaiting Chen <ktchen14@gmail.com>
2025-03-26 12:06:38 -0400

    Show indirect edges

--- source/main.c
+++ source/main.c
@@ -109,8 +109,8 @@ int main(int argc, char *argv[argc]) {
     for (size_t i = 0; i < debug_induce->universe.length; i++) {
       induce_edge_t edge = debug_induce->universe.data[i];
 
-      if (edge.indirect == 1)
-        continue;
+      /* if (edge.indirect == 1) */
+      /*   continue; */
 
       debug("%*s", 2, "");
 
