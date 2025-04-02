74b943cc3628104f39b875787a4492cc179e9485
Kaiting Chen <ktchen14@gmail.com>
2025-04-01 20:08:43 -0400

    Rewrite the detector

--- source/inductor/detect.c
+++ source/inductor/detect.c
@@ -9,6 +9,20 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+typedef struct {
+  const mu_name_t *name;
+  const mu_node_t *node;
+} item_t;
+
+typedef struct roster_t roster_t;
+struct roster_t {
+  roster_t *parent;
+  const mu_node_t *origin;
+  size_t length;
+  size_t volume;
+  item_t data[];
+};
+
 detect_t *detect_initialize(
     detect_t *detect, const mu_engine_t *engine, mu_status_t *status) {
   size_t length = engine->node_number;
@@ -27,86 +41,152 @@ detect_t *detect_initialize(
   return detect;
 }
 
-const mu_node_t *sequence_expr_get(
-    const mu_sequence_expr_t *expr, const mu_name_t *name) {
-  for (size_t i = 0; i < expr->argc; i++) {
-    const mu_stmt_t *stmt = expr->argv[i];
+__attribute__((malloc))
+static roster_t *roster_create(
+    roster_t *roster, size_t volume, const mu_node_t *origin) {
+  size_t size;
+  if (rare((size = struct_size(roster_t, data, volume))) == 0)
+    return errno = ENOMEM, NULL;
 
-    const mu_define_stmt_t *define_stmt;
-    if ((define_stmt = mu_stmt_cast(stmt, define_stmt)) == NULL)
-      continue;
+  roster_t *result;
+  if ((result = malloc(size)) == NULL)
+    return NULL;
+  *result = (roster_t) { .parent = roster, .volume = volume, .origin = origin };
+  return result;
+}
 
-    if (define_stmt->name != name)
-      continue;
-
-    return &define_stmt->as_node;
-  }
-
-  for (size_t j = 0; j < expr->argc; j++) {
-    const mu_stmt_t *stmt = expr->argv[j];
-
-    const mu_datatype_stmt_t *datatype_stmt;
-    if ((datatype_stmt = mu_stmt_cast(stmt, datatype_stmt)) != NULL) {
-      for (size_t i = 0; i < datatype_stmt->argc; i++) {
-        const mu_datatype_option_t *option = datatype_stmt->argv[i];
-        if (option->name == name)
-          return &option->as_node;
-      }
+__attribute__((nonnull))
+static const mu_node_t *roster_search(roster_t *roster, const mu_name_t *name) {
+  do {
+    for (size_t i = 0; i < roster->length; i++) {
+      if (roster->data[i].name == name)
+        return roster->data[i].node;
     }
-  }
+  } while ((roster = roster->parent) != NULL);
 
   return NULL;
 }
 
-detect_t *detect_node(detect_t *detect, const mu_node_t *node) {
+__attribute__((nonnull))
+static inline void announce(
+    roster_t *roster, const mu_name_t *name, const mu_node_t *node) {
+  assert(roster->length < roster->volume);
+  item_t item = { .name = name, .node = node };
+  roster->data[roster->length++] = item;
+}
+
+__attribute__((nonnull))
+static void view_announce(roster_t *roster, const mu_view_t *root) {
+  const mu_node_t *node = &root->as_node, *next;
   do {
-    const mu_node_t *next;
     while ((next = node_at(node, node_cursor(node)->i++)) != NULL)
       node = node_continue(node, next);
 
-    const mu_node_t *source;
-    const mu_name_t *name;
+    switch ON_ABSTRACT_OBJECT(node) {
+      case IS_KIND_OF(variable_view):
+        announce(roster, variable_view->name, &variable_view->as_node);
+        break;
 
-    const mu_name_expr_t *name_expr;
-    const mu_switch_case_t *switch_case;
-    if ((name_expr = mu_node_cast(node, name_expr)) != NULL) {
-      name = name_expr->name;
-      source = &name_expr->as_node;
-    } else if ((switch_case = mu_node_cast(node, switch_case)) != NULL) {
-      name = switch_case->name;
-      source = &switch_case->as_node;
-    } else
-      continue;
+      default: break;
+    }
+  } while ((node = node_return(node)) != NULL);
+}
 
-    const mu_node_t *anterior = node_cursor(node)->anterior;
-    for (; anterior != NULL; anterior = node_cursor(anterior)->anterior) {
-      const mu_sequence_expr_t *sequence_expr;
-      if ((sequence_expr = mu_node_cast(anterior, sequence_expr)) != NULL) {
-        const mu_node_t *target;
-        if ((target = sequence_expr_get(sequence_expr, name)) != NULL) {
-          detect->result->data[source->id] = target;
-          goto next;
+__attribute__((nonnull(2)))
+roster_t *handle_sequence_expr(
+    roster_t *roster, const mu_sequence_expr_t *sequence_expr) {
+  size_t announce_length = 0;
+  for (size_t i = 0; i < sequence_expr->argc; i++) {
+    const mu_stmt_t *stmt = sequence_expr->argv[i];
+    announce_length += node_announce_length(&stmt->as_node);
+  }
+
+  if ((roster = roster_create(roster, announce_length, &sequence_expr->as_node)) == NULL)
+    return NULL;
+
+  for (size_t i = 0; i < sequence_expr->argc; i++) {
+    const mu_stmt_t *stmt = sequence_expr->argv[i];
+
+    switch ON_ABSTRACT_OBJECT(stmt) {
+      case IS_KIND_OF(datatype_stmt):
+        announce(roster, datatype_stmt->name, &datatype_stmt->as_node);
+        for (size_t j = 0; j < datatype_stmt->argc; j++) {
+          const mu_datatype_option_t *option = datatype_stmt->argv[j];
+          announce(roster, option->name, &option->as_node);
+        }
+        break;
+
+      case IS_KIND_OF(define_stmt):
+        announce(roster, define_stmt->name, &define_stmt->expr->as_node);
+        break;
+
+      default: break;
+    }
+  }
+
+  return roster;
+}
+
+detect_t *detect_node(detect_t *detect, const mu_node_t *root) {
+  const mu_sequence_expr_t *sequence_expr = mu_node_cast(root, sequence_expr);
+  assert(sequence_expr != NULL);
+
+  roster_t *roster = NULL;
+  if ((roster = handle_sequence_expr(roster, sequence_expr)) == NULL)
+    return NULL;
+
+  const mu_node_t *node = root, *next;
+  do {
+    while ((next = node_at(node, node_cursor(node)->i++)) != NULL) {
+      switch ON_ABSTRACT_OBJECT(node) {
+        case IS_KIND_OF(lambda_expr): {
+          size_t length = node_announce_length(&lambda_expr->argument->as_node);
+
+          roster_t *next_roster;
+          if ((next_roster = roster_create(roster, length, &lambda_expr->as_node)) == NULL)
+            return NULL;
+          roster = next_roster;
+          view_announce(roster, lambda_expr->argument);
+          break;
         }
 
-        continue;
+        case IS_KIND_OF(sequence_expr): {
+          roster_t *next_roster;
+          if ((next_roster = handle_sequence_expr(roster, sequence_expr)) == NULL)
+            return NULL;
+          roster = next_roster;
+          break;
+        }
+
+        default: break;
       }
 
-      const mu_lambda_expr_t *lambda_expr;
-      if ((lambda_expr = mu_node_cast(anterior, lambda_expr)) != NULL) {
-        const mu_view_t *view = lambda_expr->argument;
-        const mu_variable_view_t *variable_view = mu_view_cast(view, variable_view);
-        assert(variable_view != NULL);
-        if (variable_view->name == name) {
-          detect->result->data[source->id] = &variable_view->as_node;
-          goto next;
-        }
-        continue;
-      }
+      node = node_continue(node, next);
     }
 
-    assert(0);
+    if (node == roster->origin) {
+      roster_t *next_roster = roster->parent;
+      free(roster);
+      roster = next_roster;
+    }
 
-  next:;
+    switch ON_ABSTRACT_OBJECT(node) {
+      case IS_KIND_OF(name_expr): {
+        const mu_node_t *target = roster_search(roster, name_expr->name);
+        assert(target != NULL);
+        detect->result->data[name_expr->as_node.id] = target;
+        break;
+      }
+
+      case IS_KIND_OF(switch_case): {
+        const mu_node_t *target = roster_search(roster, switch_case->name);
+        assert(target != NULL);
+        detect->result->data[switch_case->as_node.id] = target;
+        break;
+      }
+
+      default: break;
+    }
   } while ((node = node_return(node)) != NULL);
 
   return detect;
