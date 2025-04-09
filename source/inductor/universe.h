46f267109a08745a910afc5dc2307a5034a26bb6
Kaiting Chen <ktchen14@gmail.com>
2025-04-09 17:19:29 -0400

    Remove a use of mu_coercion_target

--- source/inductor/universe.h
+++ source/inductor/universe.h
@@ -109,9 +109,6 @@ static inline const mu_coercion_t *edge_assign(
     type_edge_t *edge, const mu_coercion_t *coercion) {
   assert(coercion->kind != MU_EDGE_COERCION);
 
-  const mu_type_t *target = mu_coercion_target(coercion, edge->source);
-  assert(target == edge->target);
-
   if (coercion->kind == MU_INDIRECT_COERCION)
     edge->indirect = 1;
 
