3f759d3a38368254df3855612516b387e45e8ae7
Kaiting Chen <ktchen14@gmail.com>
2025-04-16 08:20:20 -0400

    Add inductor header

---
+++ header/muon/inductor.h
@@ -0,0 +1,7 @@
+#ifndef MU_INDUCTOR_H
+#define MU_INDUCTOR_H
+
+#include "inductor/core.h"  // IWYU pragma: export
+#include "inductor/type.h"  // IWYU pragma: export
+
+#endif /* MU_INDUCTOR_H */
---
+++ header/muon/inductor/core.h
@@ -0,0 +1,65 @@
+#ifndef MU_INDUCTOR_CORE_H
+#define MU_INDUCTOR_CORE_H
+
+#include "../stator/name.h"
+
+#include <stddef.h>
+
+typedef enum {
+  MU_BOOLEAN_CORE,
+  MU_INTEGER_CORE,
+  MU_LAMBDA_CORE,
+  MU_VECTOR_CORE,
+  MU_RECORD_CORE,
+  MU_CUSTOM_CORE,
+} mu_core_kind_t;
+
+typedef enum {
+  MU_COVARIANCE,
+  MU_CONTRAVARIANCE,
+  MU_INVARIANCE,
+} mu_variance_t;
+
+typedef struct {
+  const mu_name_t *name;
+  mu_variance_t variance;
+} mu_core_member_t;
+
+typedef struct induce_t induce_t;
+
+typedef struct {
+  mu_core_kind_t kind;
+  const induce_t *induce;
+  const mu_name_t *name;
+  size_t argc;
+  mu_core_member_t argv[/* argc */];
+} mu_core_t;
+
+typedef struct mu_expr_t mu_expr_t;
+typedef struct {
+  const mu_core_t *source;
+  const mu_core_t *target;
+  const mu_expr_t *expr;
+} mu_instance_t;
+
+typedef struct {
+  const mu_core_t *target;
+  const mu_core_t *source;
+  size_t argv[/* target->argc */];
+} record_instance_t;
+
+const mu_core_t *mu_simple_core(induce_t *induce, const mu_name_t *name);
+
+const mu_instance_t *mu_instance(
+    const mu_core_t *source, const mu_core_t *target, const mu_expr_t *expr)
+  __attribute__((malloc, nonnull));
+
+/// Emit debugging information on the abstract @a core to the debug stream
+void mu_core_debug(const mu_core_t *core)
+  __attribute__((nonnull));
+
+/// Emit debugging information on the abstract @a instance to the debug stream
+void mu_instance_debug(const mu_instance_t *instance)
+  __attribute__((nonnull));
+
+#endif /* MU_INDUCTOR_CORE_H */
