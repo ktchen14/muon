9712215a5342ae5e3d974f2937e1d7ecda1d62c0
Kaiting Chen <ktchen14@gmail.com>
2025-04-15 22:30:21 -0400

    more fixes

--- source/inductor/type.h
+++ source/inductor/type.h
@@ -235,10 +235,11 @@ static inline const mu_type_t *type_return(const mu_type_t *type) {
   const mu_type_t *anterior = cursor->anterior;
   *cursor = (type_cursor_t) {0};
 
-  const mu_core_type_t *core_anterior;
-  if ((core_anterior = mu_type_cast(anterior, core_anterior)) != NULL) {
+  const mu_core_type_t *core_type;
+  if (anterior != NULL && (core_type = mu_type_cast(anterior, core_type)) != NULL) {
+    const mu_core_t *core = core_type->core;
     cursor = type_cursor(anterior);
-    if (core_anterior->core->argv[cursor->i - 1].variance == MU_CONTRAVARIANCE)
+    if (core->argv[cursor->i - 1].variance == MU_CONTRAVARIANCE)
       charge = !charge;
   }
   return anterior;
