4371f62d4db4f09e5ff62be92e5d5051696019f7
Kaiting Chen <ktchen14@gmail.com>
2025-05-24 11:41:41 -0400

    Remove name global

--- source/author/node.c
+++ source/author/node.c
@@ -24,8 +24,6 @@
 
 LLVMValueRef SKIP = (void *) &(int) {1};
 
-static char name[256];
-
 static LLVMValueRef evince_result(const author_t *author, const mu_node_t *node) {
   assert(node->id < author->node_length);
   LLVMValueRef result = author->node_to_value[node->id];
@@ -312,11 +310,14 @@ LLVMModuleRef script_emit(author_t *author, const mu_node_t *root) {
         if ((lambda_ty = get_type(author, lambda_type)) == NULL)
           return NULL;
 
-        snprintf(name, sizeof(name), "lambda.%zu", lambda_expr->as_node.id);
+        char name[sizeof("lambda." ID)];
+        int e = snprintf(name, sizeof(name), "lambda.%zu", lambda_expr->as_node.id);
+        assert((size_t) e < sizeof(name));
         LLVMValueRef lambda = LLVMAddFunction(author->module, name, lambda_ty);
-        LLVMBasicBlockRef entry = LLVMAppendBasicBlock(lambda, "");
+
+        LLVMBasicBlockRef main = LLVMAppendBasicBlock(lambda, "");
         LLVMBuilderRef tail = LLVMCreateBuilder();
-        LLVMPositionBuilderAtEnd(tail, entry);
+        LLVMPositionBuilderAtEnd(tail, main);
 
         if (author_continue(author, lambda, tail) == NULL)
           return NULL;
