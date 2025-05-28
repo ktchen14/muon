64ac2ab2aff7b079c7b6793757aad9b519fd1cce
Kaiting Chen <ktchen14@gmail.com>
2025-05-28 15:31:04 -0400

    Slight rewrite of name

--- source/engine/name.c
+++ source/engine/name.c
@@ -6,13 +6,23 @@
 
 #include <errno.h>
 #include <stddef.h>
-#include <stdlib.h>
 #include <string.h>
 
 const mu_name_t *mu_name(
     mu_engine_t *engine,
     size_t length,
     const mu_char8_t text[restrict static length]) {
+  for (size_t i = 0; i < engine->name_number; i++) {
+    const mu_name_t *name = engine->name[i];
+
+    if (length != name->length)
+      continue;
+    if (memcmp(text, name->text, length) != 0)
+      continue;
+
+    return name;
+  }
+
   size_t size;
   if (rare((size = struct_size(mu_name_t, text, length + 1)) == 0))
     return errno = ENOMEM, NULL;
@@ -20,24 +30,11 @@ const mu_name_t *mu_name(
   mu_name_t *name;
   if ((name = engine_allocate(engine, size)) == NULL)
     return NULL;
-  *name = (mu_name_t) { .length = length };
+  *name = (mu_name_t) { .engine = engine, .length = length };
   memcpy(name->text, text, length);
   name->text[length] = '\0';
 
-  for (size_t i = 0; i < engine->name_number; i++) {
-    const mu_name_t *already = engine->name[i];
-    if (name->length != already->length)
-      continue;
-    if (memcmp(name->text, already->text, name->length))
-      continue;
-
-    free(name);
-    return already;
-  }
-
-  name->engine = engine;
-  engine->name[engine->name_number++] = name;
-  return name;
+  return engine->name[engine->name_number++] = name;
 }
 
 void mu_name_debug(const mu_name_t *name) {
