83b8423851ea53cce6b9f409942111d7b503b2ad
Kaiting Chen <ktchen14@gmail.com>
2025-05-28 09:48:08 -0400

    Some changes

--- header/muon.h
+++ header/muon.h
@@ -1,9 +1,10 @@
 #ifndef MUON_H
 #define MUON_H
 
-#include "muon/common.h"  // IWYU pragma: export
-#include "muon/script.h"  // IWYU pragma: export
-#include "muon/stator.h"  // IWYU pragma: export
-#include "muon/status.h"  // IWYU pragma: export
+#include "muon/common.h"    // IWYU pragma: export
+#include "muon/inductor.h"  // IWYU pragma: export
+#include "muon/script.h"    // IWYU pragma: export
+#include "muon/stator.h"    // IWYU pragma: export
+#include "muon/status.h"    // IWYU pragma: export
 
 #endif /* MUON_H */
