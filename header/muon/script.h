4b4c1e967b729e8bed183d776a377e4cd655ebf5
Kaiting Chen <ktchen14@gmail.com>
2025-09-07 13:29:57 -0400

    Make muon_scan public

--- header/muon/script.h
+++ header/muon/script.h
@@ -2,6 +2,7 @@
 #define MU_SCRIPT_H
 
 #include "engine.h"
+#include "status.h"
 
 #include <stddef.h>
 
@@ -21,6 +22,12 @@ MuonSequenceExpr *mu_script_to_sequence_expr_with_prefix(
     const mu_script_t *script,
     size_t length,
     MuonStmt *prefix[]);
+
+typedef unsigned char mu_char8_t;
+
+mu_script_t *muon_scan(
+    MuonEngine *engine, mu_status_t *status, const mu_char8_t *buffer)
+  __attribute__((nonnull));
 
 void mu_script_debug(const mu_script_t *script) __attribute__((nonnull));
 
--- source/script.h
+++ source/script.h
@@ -3,13 +3,4 @@
 
 #include <muon/script.h>  // IWYU pragma: export
 
-#include "engine.h"
-#include "status.h"
-
-typedef unsigned char mu_char8_t;
-
-mu_script_t *muon_scan(
-    MuonEngine *engine, mu_status_t *status, const mu_char8_t *buffer)
-  __attribute__((nonnull));
-
 #endif /* MU_SCRIPT_I */
