8039f5d514b4e3387c2af3dd52fbfdebcf510fb7
Kaiting Chen <ktchen14@gmail.com>
2025-09-07 11:11:43 -0400

    Remove scan_debug

--- source/script.h
+++ source/script.h
@@ -8,9 +8,6 @@
 
 typedef unsigned char mu_char8_t;
 
-void scan_debug(const unsigned char *string, const char *name)
-  __attribute__((nonnull(1)));
-
 mu_script_t *mu_read_script(
     MuonEngine *engine, mu_status_t *status, const mu_char8_t *buffer)
   __attribute__((nonnull));
--- source/script/parser.c
+++ source/script/parser.c
@@ -2,8 +2,9 @@
 #include "symbol.h"
 #include "scan.h"
 
-#include "../script.h"
+#include "../common.h"
 #include "../engine.h"
+#include "../script.h"
 #include "../status.h"
 
 #include <assert.h>
@@ -11,16 +12,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-void scan_debug(const YYCTYPE *string, const char *name) {
-  scan_t scan = {0};
-  yytoken_kind_t kind;
-  YYSTYPE yylval;
-  YYLTYPE yylloc;
-
-  while ((kind = scan_next(string, &scan, &yylval, &yylloc)) != YYEOF)
-    symbol_debug(stderr, kind, &yylval, &yylloc);
-}
 
 mu_script_t *mu_read_script(
     MuonEngine *engine, mu_status_t *status, const mu_char8_t *string) {
@@ -33,15 +24,27 @@ mu_script_t *mu_read_script(
     return errno = ENOMEM, NULL;
 
   int e;
+  yytoken_kind_t kind;
   do {
     YYSTYPE yylval;
     YYLTYPE yylloc;
-    yytoken_kind_t kind = scan_next(string, &scan, &yylval, &yylloc);
-    symbol_debug(stderr, kind, &yylval, &yylloc);
+    kind = scan_next(string, &scan, &yylval, &yylloc);
+
+    if (debug_scan)
+      symbol_debug(stderr, kind, &yylval, &yylloc);
+
     e = yypush_parse(pstate, kind, &yylval, &yylloc, &syntax);
   } while (e == YYPUSH_MORE);
 
   yypstate_delete(pstate);
+
+  if (kind != YYEOF && debug_scan) {
+    yytoken_kind_t kind;
+    YYSTYPE yylval;
+    YYLTYPE yylloc;
+    while ((kind = scan_next(string, &scan, &yylval, &yylloc)) != YYEOF)
+      symbol_debug(stderr, kind, &yylval, &yylloc);
+  }
 
   if ((errno = ((int[]) {0, EINVAL, ENOMEM})[e]) != 0)
     fprintf(stderr, "Error %i\n", e);
